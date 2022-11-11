2ee2d9703881b96ab7858210f000a73e8985033f
Jesse Ilescas <ilesmj9516@gmail.com>
2022-11-10 20:50:47 -0600

    express server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Funcionado nodemon');
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 22
+const { Router } = require('express')
+const expressRouter = Router()
+
+app.use(express.static('public'))
+
+expressRouter.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/public/index.html')
+})
+
+
+const server = app.listen(PORT, (req, res) =>{
+    console.log(`Servidor escuchando en el puerto : ${server.address().port}`);
+})
+
+app.use('/inicio', expressRouter)
