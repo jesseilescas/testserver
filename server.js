2f50177f91acad41cf2956957ceb43e79c0a6a69
Jesse Ilescas <ilesmj9516@gmail.com>
2022-11-10 21:06:38 -0600

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 22
+const PORT = process.env.PORT
 const { Router } = require('express')
 const expressRouter = Router()
 
